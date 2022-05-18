603263e6d05d4d0ff630f210e0c9cfa6c11bbcc3
João Paulo de Souza <joao_paulo767@hotmail.com>
2022-05-17 22:48:34 -0300

    Adicionadas mais contantes de cores e metodo para converter rgb para hexadecimal

--- utils/__init__.py
+++ utils/__init__.py
@@ -13,6 +13,12 @@ class Utils:
     def rgb_to_hex(r: int, g: int, b: int):
         return '#{:X}{:X}{:X}'.format(r, g, b)
 
+    @staticmethod
+    def rgb_tuple_to_rex(rgb: tuple) -> str:
+        if len(rgb) != 3:
+            return ''
+        return '#{:X}{:X}{:X}'.format(rgb[0], rgb[1], rgb[2])
+
 
 class Consts:
     DEFAULT_SCREEN_WIDTH = 800
@@ -23,6 +29,19 @@ class Consts:
 
 class Colors:
     BLACK = (0, 0, 0)
-    BLACK_HEX = Utils.rgb_to_hex(0, 0, 0)
     WHITE = (255, 255, 255)
-    WHITE_HEX = Utils.rgb_to_hex(255, 255, 255)
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+    GREEN = (0, 255, 0)
+    YELLOW = (255, 255, 0)
+    ORANGE = (255, 127, 0)
+    PURPLE = (255, 0, 255)
+    PINK = (255, 0, 127)
+    GRAY = (127, 127, 127)
+    DARK_BLUE = (0, 0, 127)
+    DARK_RED = (127, 0, 0)
+    DARK_GREEN = (0, 127, 0)
+    LIGHT_GREEN = (127, 255, 0)
+    LIGHT_RED = (255, 80, 80)
+    LIGHT_BLUE = (0, 200, 255)
+    LIME = (0, 255, 127)
